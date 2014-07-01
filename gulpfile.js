0777a46b3422142bda12ea195619db109ce71754
mdixon47 <mdixon@rap.midco.net>
2014-07-01 11:55:42 -0600

    Minified the HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-HTML'),
     concat = require('gulp-concat');
 
 var env,
@@ -17,7 +18,7 @@ var env,
     outputDir,
     sassStyle;
 
-env = process.env.NODE_ENV || 'development';
+env = process.env.NODE_ENV || 'production';
 
 if (env==='development') {
   outputDir = 'builds/development/';
@@ -72,7 +73,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
 
@@ -84,7 +85,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+  	.pipe (gulpif(env === 'production', minifyHTML()))
+  	.pipe (gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
 
