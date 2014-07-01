c9c38ed972960d51b77b82751d25751cb2080b20
mdixon47 <mdixon@rap.midco.net>
2014-06-30 22:20:20 -0600

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require ('gulp');
-	gutil = require ('gulp-util');
+var gulp = require ('gulp'),
+	gutil = require ('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function () {
 
+
+gulp.task('coffee', function () {
+	gulp.src('components/coffee/tagline.coffee')
+	.pipe(coffee({ bare: true })
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
