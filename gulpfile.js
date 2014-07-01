e5f77516e80a3274fa66d08ad7ae44f5886cb722
mdixon47 <mdixon@rap.midco.net>
2014-07-01 09:07:55 -0600

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var jsSources =[
 	];
 
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json']
 
 gulp.task('coffee', function () {
 	gulp.src(coffeeSources)
@@ -49,9 +51,11 @@ gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task ('default', ['coffee', 'js', 'compass', 'connect','watch']);
+gulp.task ('default', [ 'coffee','html', 'js', 'compass', 'connect','watch','json']);
 
 
 gulp.task ('connect', function(){
@@ -59,4 +63,14 @@ gulp.task ('connect', function(){
 		root: 'builds/development/',
 		livereload: true
 	});
+});
+
+gulp.task ('html', function(){
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+});
+
+gulp.task ('json', function(){
+	gulp.src(jsonSources)
+	.pipe(connect.reload())
 });
