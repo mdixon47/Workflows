575dcf92cf0a1ef9af017cc8e2c4c7a412bafaa7
mdixon47 <mdixon@rap.midco.net>
2014-07-01 01:56:47 -0600

    Added Live Reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require ('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat= require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -27,6 +28,7 @@ gulp.task('js', function (){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task('compass', function (){
@@ -39,6 +41,8 @@ gulp.task('compass', function (){
 		}))
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload())
+		
 });
 
 gulp.task('watch', function(){
@@ -47,4 +51,12 @@ gulp.task('watch', function(){
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task ('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task ('default', ['coffee', 'js', 'compass', 'connect','watch']);
+
+
+gulp.task ('connect', function(){
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
